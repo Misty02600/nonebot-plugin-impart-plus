d000fb33813462f8a75c1f3401f90ca0941e4118
Misty02600 <xiao02600@gmail.com>
2026-08-30 19:08:24 +0800

    feat: 迁移插件源码

--- src/nonebot_plugin_impart_plus/__init__.py
+++ src/nonebot_plugin_impart_plus/__init__.py
@@ -1,24 +1,99 @@
-# from nonebot import logger, require
-from nonebot.plugin import PluginMetadata  # , inherit_supported_adapters
+"""插件入口"""
 
-# require("nonebot_plugin_uninfo")
-# require("nonebot_plugin_alconna")
-# require("nonebot_plugin_localstore")
-# require("nonebot_plugin_apscheduler")
+from re import I
+
+from nonebot import get_driver, on_command, on_regex, require
+from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
+from nonebot.permission import SUPERUSER
+from nonebot.plugin import PluginMetadata
+
 from .config import Config
+from .data_sheet import init_db
+from .handle import impart
 
-__all__ = ["__plugin_meta__", "handlers"]
+require("nonebot_plugin_apscheduler")
+from nonebot_plugin_apscheduler import scheduler
 
-__plugin_meta__ = PluginMetadata(
-    name="名称",
-    description="描述",
-    usage="模板测试 [文本]",
-    type="application",  # application: 功能性插件 | library: 库插件
-    homepage="https://github.com/Misty02600/nonebot_plugin_impart_plus",
-    config=Config,
-    # supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
-    supported_adapters={"~onebot.v11"},
-    extra={"author": "Misty02600"},
+
+@get_driver().on_startup
+async def init_plugin() -> None:
+    await init_db()
+
+
+scheduler.add_job(
+    impart.penalties_and_resets,
+    "cron",
+    hour=0,
+    misfire_grace_time=600,
 )
 
-from . import handlers
+__plugin_meta__ = PluginMetadata(
+    name="nonebot_plugin_impart_plus",
+    usage="使用<银趴帮助/impart help>指令获取使用说明",
+    description="NoneBot2 银趴插件 Plus",
+    type="application",
+    homepage="https://github.com/Misty02600/nonebot_plugin_impart_plus",
+    config=Config,
+    supported_adapters={"~onebot.v11"},
+    extra={
+        "priority": 20,
+    },
+)
+
+on_command(
+    "pk",
+    aliases={"对决"},
+    rule=Config.rule,
+    priority=20,
+    block=False,
+    handlers=[impart.pk],
+)
+
+on_regex("^(打胶|开导)$", priority=20, block=True, handlers=[impart.dajiao])
+
+on_command(
+    "嗦牛子", aliases={"嗦", "suo"}, priority=20, block=True, handlers=[impart.suo]
+)
+
+on_command("查询", priority=20, block=False, handlers=[impart.queryjj])
+
+on_regex(
+    r"^(jj|牛牛)(排行榜|排名|榜单|rank)",
+    flags=I,
+    priority=20,
+    block=True,
+    handlers=[impart.jjrank],
+)
+
+on_regex(
+    r"^(日群友|日群主|日管理|透群友|透群主|透管理)",
+    flags=I,
+    priority=20,
+    block=True,
+    handlers=[impart.yinpa],
+)
+
+on_regex(
+    r"^(开始|开启|关闭|禁止)(银趴|impart)",
+    permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER,
+    flags=I,
+    priority=10,
+    block=True,
+    handlers=[impart.open_module],
+)
+
+on_command(
+    "注入查询",
+    aliases={"摄入查询", "射入查询"},
+    priority=20,
+    block=True,
+    handlers=[impart.query_injection],
+)
+
+on_regex(
+    r"^(银趴|impart)(介绍|帮助)$",
+    flags=I,
+    priority=20,
+    block=True,
+    handlers=[impart.yinpa_introduce],
+)
--- tests/units/config_test.py
+++ tests/units/config_test.py
@@ -1,4 +1,9 @@
 def test_config_can_be_instantiated():
     from nonebot_plugin_impart_plus.config import Config
 
-    assert Config().model_dump() == {}
+    config = Config()
+
+    assert config.dj_cd_time == 300
+    assert config.pk_cd_time == 60
+    assert config.suo_cd_time == 300
+    assert config.fuck_cd_time == 3600
